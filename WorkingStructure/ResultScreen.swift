4b0ca7d6e1aab0ff44cff8a93ebc63d4f2869db4
aydinnkaya <aydinnkaya1@gmail.com>
2024-12-18 10:07:13 +0300

    ProjectCreated

---
+++ WorkingStructure/ResultScreen.swift
@@ -0,0 +1,29 @@
+//
+//  ResultScreen.swift
+//  WorkingStructure
+//
+//  Created by AydınKaya on 18.12.2024.
+//
+
+import UIKit
+
+class ResultScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
